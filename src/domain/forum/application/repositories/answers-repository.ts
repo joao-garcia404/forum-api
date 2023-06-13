3f48ecf06ec3d38b9d2e93605e4737a80f3442c6
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-06-12 21:50:41 -0300

    Feat: Edit answer use-case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -3,5 +3,6 @@ import { Answer } from '../../enterprise/entities/answer';
 export interface AnswersRepository {
   findById(id: string): Promise<Answer | null>;
   create(anwser: Answer): Promise<void>;
+  save(anwser: Answer): Promise<void>;
   delete(anwser: Answer): Promise<void>;
 }
---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,51 @@
+import { EditAnswerUseCase } from './edit-answer';
+
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository';
+import { makeAnswer } from 'test/factories/make-answer';
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: EditAnswerUseCase;
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository);
+  });
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer({
+      authorId: new UniqueEntityID('author-1')
+    }, new UniqueEntityID('answer-1'));
+
+    await inMemoryAnswersRepository.create(newAnswer);
+
+    await sut.execute({
+      answerId: newAnswer.id.toString(),
+      authorId: 'author-1',
+      content: 'Test content'
+    });
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'Test content',
+    })
+  });
+
+  it('Should not be able to edit a answer from another user.', async () => {
+    const newAnswer = makeAnswer({
+      authorId: new UniqueEntityID('author-1')
+    }, new UniqueEntityID('answer-1'));
+
+    await inMemoryAnswersRepository.create(newAnswer);
+
+    expect(() => {
+      return sut.execute({
+        answerId: 'answer-1',
+        authorId: 'author-2',
+        content: 'Test content'
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+});
+
