af49b2dd8be12d535f66e5c096401ba98a32b873
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-06-24 18:25:00 -0300

    Feat: Delete question comment use-case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -5,7 +5,21 @@ import { QuestionComment } from "@/domain/forum/enterprise/entities/question-com
 export class InMemoryQuestionCommentsRepository implements QuestionCommentRepository {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    const questionComment = this.items.find((item) => item.id.toString() === id);
+
+    if (!questionComment) return null;
+
+    return questionComment;
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
+  }
+
+  async delete(question: QuestionComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items.splice(itemIndex, 1);
   }
 }
