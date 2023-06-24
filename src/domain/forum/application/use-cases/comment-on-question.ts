af9959dd8edf3dd67c4b40a57cf57a466921ba76
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-06-24 18:12:13 -0300

    Feat: Question and Answer comments use cases

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,46 @@
+import { QuestionsRepository } from '../repositories/questions-repository';
+import { QuestionCommentRepository } from '../repositories/question-comments-repository';
+
+import { QuestionComment } from '../../enterprise/entities/question-comment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionsCommentRepository: QuestionCommentRepository,
+  ) { }
+
+  async execute({
+    authorId,
+    content,
+    questionId,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error('Question not found.');
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    });
+
+    await this.questionsCommentRepository.create(questionComment)
+
+    return {
+      questionComment,
+    }
+  }
+}
