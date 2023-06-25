dc96c57a3f25cb650c2b90b13941277e9b99d17a
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-06-24 22:59:37 -0300

    Refactor: Starting function error handling at use-cases

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,11 +1,19 @@
+import { Either, left, right } from '@/core/either';
+
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository';
+
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { NotAllowedError } from './errors/not-allowed-error';
 
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string;
   answerCommentId: string;
 }
 
-interface DeleteAnswerCommentUseCaseResponse {}
+type DeleteAnswerCommentUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerCommentUseCase {
   constructor(
@@ -19,15 +27,15 @@ export class DeleteAnswerCommentUseCase {
     const answerComment = await this.answersCommentsRepository.findById(answerCommentId);
 
     if (!answerComment) {
-      throw new Error('Answer comment not found.');
+      return left(new ResourceNotFoundError());
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed')
+      return left(new NotAllowedError())
     }
 
     await this.answersCommentsRepository.delete(answerComment)
 
-    return {}
+    return right({})
   }
 }
