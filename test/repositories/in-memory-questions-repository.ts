a18175dd174fff28d0e040ee5ddde2ad07f83739
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-09-30 13:45:33 -0300

    Feat: Question best answer notification event

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,9 @@
-import { PaginationParams } from "@/core/repositories/pagination-params";
-import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/questions-attachments-repository";
+import { DomainEvents } from '@/core/events/domain-events';
+import { PaginationParams } from '@/core/repositories/pagination-params';
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/questions-attachments-repository';
 
-import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
-import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
+import { Question } from '@/domain/forum/enterprise/entities/question';
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
@@ -30,7 +31,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async findManyRecent({ page }: PaginationParams) {
     const questions = this.items
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      .slice((page - 1) * 20, page * 20)
+      .slice((page - 1) * 20, page * 20);
 
     return questions;
   }
@@ -43,6 +44,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const itemIndex = this.items.findIndex((item) => item.id === question.id);
 
     this.items[itemIndex] = question;
+
+    DomainEvents.dispatchEventsForAggregate(question.id);
   }
 
   async delete(question: Question) {
